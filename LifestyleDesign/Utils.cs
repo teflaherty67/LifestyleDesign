06a7cc3258173dfe4ad879c82a9c7133e426aeba
Tom E Flaherty <teflaherty@live.com>
2022-12-16 16:43:23 -0600

    Add project files.

---
+++ LifestyleDesign/Utils.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifestyleDesign
+{
+    internal static class Utils
+    {
+        internal static void method1()
+        {
+            // add method code here
+        }
+    }
+}

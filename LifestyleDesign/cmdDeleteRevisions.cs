62534ff2f686379fe1f492a5389487554fdbc1b8
Tom E Flaherty <teflaherty@live.com>
2022-12-21 09:44:11 -0600

    step 1 Delete Revisions complete

---
+++ LifestyleDesign/cmdDeleteRevisions.cs
@@ -0,0 +1,47 @@
+#region Namespaces
+using Autodesk.Revit.ApplicationServices;
+using Autodesk.Revit.Attributes;
+using Autodesk.Revit.DB;
+using Autodesk.Revit.UI;
+using Autodesk.Revit.UI.Selection;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+
+#endregion
+
+namespace LifestyleDesign
+{
+    [Transaction(TransactionMode.Manual)]
+    public class cmdDeleteRevisions : IExternalCommand
+    {
+        public Result Execute(
+          ExternalCommandData commandData,
+          ref string message,
+          ElementSet elements)
+        {
+            UIApplication uiapp = commandData.Application;
+            UIDocument uidoc = uiapp.ActiveUIDocument;
+            Application app = uiapp.Application;
+            Document doc = uidoc.Document;
+
+            // get all the revisions in the project
+
+            IList<ElementId> revisions = Revision.GetAllRevisionIds(doc);
+
+            TaskDialog.Show("Results", "There are " + revisions.Count() + " revisions in the project.");
+
+            // set the first revision as Issued
+
+
+
+            // delete all the remaining revisions
+
+            // uncheck the Issued box on the first revision
+                       
+
+            return Result.Succeeded;
+        }
+    }
+}

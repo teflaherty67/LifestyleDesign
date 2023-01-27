04024f913acad4abbc260308ccf46f2cbf0b790d
Tom E Flaherty <teflaherty@utexas.edu>
2023-01-27 17:28:43 -0600

    ribbon in progress

--- LifestyleDesign/App.cs
+++ LifestyleDesign/App.cs
@@ -5,6 +5,11 @@ using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
+using System.Reflection;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using System.IO;
 
 #endregion
 
@@ -12,8 +17,29 @@ namespace LifestyleDesign
 {
     internal class App : IExternalApplication
     {
-        public Result OnStartup(UIControlledApplication a)
+        public Result OnStartup(UIControlledApplication app)
         {
+            string assemblyName = Utils.GetAssemblyName();
+
+            // create the ribbon tab
+
+            try
+            {
+                app.CreateRibbonTab("Lifestyle Design");
+            }
+            catch (Exception)
+            {
+                Debug.Print("Tab already exists");
+            }
+
+            // create ribbon panel
+
+            RibbonPanel panel1 = Utils.CreateRibbonPanel(app, "Lifestyle Design", "Mirror Plan");
+            RibbonPanel panel2 = Utils.CreateRibbonPanel(app, "Lifestyle Design", "Project Management");
+
+            // create button data instances
+
+
             return Result.Succeeded;
         }
 
--- LifestyleDesign/Utils.cs
+++ LifestyleDesign/Utils.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using Autodesk.Revit.ApplicationServices;
@@ -8,6 +10,8 @@ using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
+using System.Drawing;
+using System.Windows.Media.Imaging;
 
 namespace LifestyleDesign
 {
@@ -80,6 +84,49 @@ namespace LifestyleDesign
 
                 param.Set(value);
             }
+        }
+
+        internal static BitmapImage BitmapToImageSource(Bitmap bm)
+        {
+            using (MemoryStream mem = new MemoryStream())
+            {
+                bm.Save(mem, System.Drawing.Imaging.ImageFormat.Png);
+                mem.Position = 0;
+                BitmapImage bmi = new BitmapImage();
+                bmi.BeginInit();
+                bmi.StreamSource = mem;
+                bmi.CacheOption = BitmapCacheOption.OnLoad;
+                bmi.EndInit();
+
+                return bmi;
+            }
+        }
+
+        internal static RibbonPanel CreateRibbonPanel(UIControlledApplication app, string tabName, string panelName)
+        {
+            RibbonPanel curPanel = GetRibbonPanelByName(app, tabName, panelName);
+
+            if (curPanel == null)
+                curPanel = app.CreateRibbonPanel(tabName, panelName);
+
+            return curPanel;
+        }
+
+        private static RibbonPanel GetRibbonPanelByName(UIControlledApplication app, string tabName, string panelName)
+        {
+            foreach (RibbonPanel tempPanel in app.GetRibbonPanels(tabName))
+            {
+                if (tempPanel.Name == panelName)
+                    return tempPanel;
+            }
+
+            return null;
+        }
+
+        internal static string GetAssemblyName()
+        {
+            string assemblyName = Assembly.GetExecutingAssembly().Location;
+            return assemblyName;
         }
     }
 }

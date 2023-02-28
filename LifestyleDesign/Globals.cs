31af1e961aa902959b94303ed62111ca7199cd5a
Tom E Flaherty <teflaherty@utexas.edu>
2023-02-27 22:15:22 -0600

    Revision Journal in progress

---
+++ LifestyleDesign/Globals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LifestyleDesign
+{
+    internal static class Globals
+    {
+    }
+}
---
+++ LifestyleDesign/RevisionJournal.xaml.cs
@@ -0,0 +1,28 @@
+﻿using Autodesk.Revit.UI;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace LifestyleDesign
+{
+    /// <summary>
+    /// Interaction logic for RevisionJournal.xaml
+    /// </summary>
+    public partial class RevisionJournal : Window
+    {
+        
+    }
+}

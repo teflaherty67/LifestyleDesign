3ccd75dada220a56368c5c8d56679efe96452c43
Tom E Flaherty <teflaherty@utexas.edu>
2023-02-28 23:07:55 -0600

    in progress

--- LifestyleDesign/RevisionJournal.xaml.cs
+++ LifestyleDesign/RevisionJournal.xaml.cs
@@ -23,6 +23,151 @@ namespace LifestyleDesign
     /// </summary>
     public partial class RevisionJournal : Window
     {
-        
+        string todoFilePath = "";
+        BindingList<JournalData> todoDataList = new BindingList<JournalData>();
+        JournalData curEdit;
+
+        public RevisionJournal(string filePath)
+        {
+            InitializeComponent();
+
+            tbFileNmae.Text = System.IO.Path.GetFileName(filePath);
+
+            string curPath = System.IO.Path.GetDirectoryName(filePath);
+            string curFileName = System.IO.Path.GetFileNameWithoutExtension(filePath) + "_ToDo.txt";
+
+            todoFilePath = curPath + @"\" + curFileName;
+
+            ReadToDoFile();
+        }
+
+        private void ReadToDoFile()
+        {
+            if (File.Exists(todoFilePath))
+            {
+                int counter = 0;
+                string[] strings = File.ReadAllLines(todoFilePath);
+
+                foreach (string line in strings)
+                {
+                    string[] todoData = JournalData.ParseDsiplayString(line);
+
+                    JournalData curToDo = new JournalData(counter + 1, todoData[0], todoData[1]);
+                    todoDataList.Add(curToDo);
+                    counter++;
+                }
+            }
+
+            ShowData();
+        }
+
+        private void ShowData()
+        {
+            lbxTasks.ItemsSource = null;
+            lbxTasks.ItemsSource = todoDataList;
+            lbxTasks.DisplayMemberPath = "Display";
+        }
+
+        private void btnAddEdit_Click(object sender, RoutedEventArgs e)
+        {
+
+            if (curEdit == null)
+            {
+                AddToDoItem(tbxItem.Text);
+            }
+            else
+            {
+                CompleteEditingItem();
+            }
+
+            tbxItem.Text = "";
+        }
+
+        private void AddToDoItem(string todoText)
+        {
+            JournalData curToDo = new JournalData(todoDataList.Count + 1, todoText, "To Do");
+            todoDataList.Add(curToDo);
+
+            WriteToDoFile();
+        }
+
+        private void WriteToDoFile()
+        {
+            using (StreamWriter writer = File.CreateText(todoFilePath))
+            {
+                foreach (JournalData curToDo in lbxTasks.Items)
+                {
+                    curToDo.UpdateDisplayString();
+                    writer.WriteLine(curToDo.Display);
+                }
+            }
+
+            ShowData();
+        }
+
+        private void CompleteEditingItem()
+        {
+            foreach (JournalData todo in todoDataList)
+            {
+                if (todo == curEdit)
+                    todo.Text = tbxItem.Text;
+            }
+
+            curEdit = null;
+            tbkAddEdit.Text = "Add Item";
+            btnAddEdit.Content = "Add Item";
+
+            WriteToDoFile();
+        }
+
+        private void btnEdit_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxTasks.SelectedItems != null)
+            {
+                JournalData curToDo = lbxTasks.SelectedItem as JournalData;
+                StartEditingItem(curToDo);
+            }
+        }
+
+        private void StartEditingItem(JournalData curToDo)
+        {
+            curEdit = curToDo;
+
+            tbkAddEdit.Text = "Update Item";
+            btnAddEdit.Content = "Update Item";
+            tbxItem.Text = curToDo.Text;
+        }
+
+        private void btnDelete_Click(object sender, RoutedEventArgs e)
+        {
+            if (lbxTasks.SelectedItems != null)
+            {
+                JournalData curToDo = lbxTasks.SelectedItem as JournalData;
+                RemoveItem(curToDo);
+            }
+        }
+
+        private void RemoveItem(JournalData curToDo)
+        {
+            todoDataList.Remove(curToDo);
+            ReOrderToDoItems();
+
+            WriteToDoFile();
+        }
+
+        private void ReOrderToDoItems()
+        {
+            for (int i = 0; i < todoDataList.Count; i++)
+            {
+                todoDataList[i].PositionNumber = i + 1;
+                todoDataList[i].UpdateDisplayString();
+            }
+            WriteToDoFile();
+        }
+
+        private void btnClose_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
     }
 }

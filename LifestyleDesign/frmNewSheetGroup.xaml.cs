155cad5a885ad7f2f03b2e2a0e6440045bcff57e
Tom E Flaherty <teflaherty@utexas.edu>
2023-03-04 17:58:44 -0600

    form design complete

---
+++ LifestyleDesign/frmNewSheetGroup.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace LifestyleDesign
+{
+    /// <summary>
+    /// Interaction logic for frmNewSheetGroup.xaml
+    /// </summary>
+    public partial class frmNewSheetGroup : Window
+    {
+        public frmNewSheetGroup()
+        {
+            InitializeComponent();
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}

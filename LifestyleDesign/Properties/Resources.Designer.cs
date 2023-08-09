c94737a2125c25824649628bf24cfab9b3e889be
Tom E Flaherty <teflaherty@utexas.edu>
2023-08-09 16:27:39 -0500

    code cleanup

--- LifestyleDesign/Properties/Resources.Designer.cs
+++ LifestyleDesign/Properties/Resources.Designer.cs
@@ -63,46 +63,6 @@ namespace LifestyleDesign.Properties {
         /// <summary>
         ///   Looks up a localized resource of type System.Drawing.Bitmap.
         /// </summary>
-        internal static System.Drawing.Bitmap Check_16 {
-            get {
-                object obj = ResourceManager.GetObject("Check_16", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Check_32 {
-            get {
-                object obj = ResourceManager.GetObject("Check_32", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Clear_16 {
-            get {
-                object obj = ResourceManager.GetObject("Clear_16", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap Clear_32 {
-            get {
-                object obj = ResourceManager.GetObject("Clear_32", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
         internal static System.Drawing.Bitmap DeleteRevisions_16 {
             get {
                 object obj = ResourceManager.GetObject("DeleteRevisions_16", resourceCulture);

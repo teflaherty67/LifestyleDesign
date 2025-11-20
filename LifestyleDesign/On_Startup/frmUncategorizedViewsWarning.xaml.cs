d508de93bacd620663fde8670da7e2101d2ae281
Tom E Flaherty <67797311+teflaherty67@users.noreply.github.com>
2025-11-20 17:08:56 -0600

    cleanup

--- LifestyleDesign/On_Startup/frmUncategorizedViewsWarning.xaml.cs
+++ LifestyleDesign/On_Startup/frmUncategorizedViewsWarning.xaml.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Windows;
 
-namespace LifestyleDesign.On_Startup
+namespace LifestyleDesign
 {
     public partial class frmUncategorizedViewsWarning : Window
     {

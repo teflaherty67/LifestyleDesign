3efc5a864962f545c0f3aa9627818b1fece36bb8
ThomasEFlaherty <ThomasEFlaherty@LUSAD_LAPTOP>
2026-03-17 15:53:54 -0500

    Create Schedules correction

--- LifestyleDesign/Delete_Backups/frmDeleteBackups.xaml.cs
+++ LifestyleDesign/Delete_Backups/frmDeleteBackups.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using LifestyleDesign.Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -19,9 +20,61 @@ namespace LifestyleDesign
     /// </summary>
     public partial class frmDeleteBackups : Window
     {
+        // public properties to expose form values
+        public string SelectedFolder { get; set; }
+        public bool IncludeSubfolders { get; set; }
+
         public frmDeleteBackups()
         {
             InitializeComponent();
+        }
+        private void btnSelect_Click(object sender, RoutedEventArgs e)
+        {
+            using (FolderBrowserDialog dialog = new FolderBrowserDialog())
+            {
+                dialog.Description = "Select Folder";
+                dialog.ShowNewFolderButton = false;
+                dialog.SelectedPath = @"S:\Shared Folders\Lifestyle USA Design";
+
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    tbxFolder.Text = dialog.SelectedPath;
+                }
+            }
+        }
+
+        private void btnOK_Click(object sender, RoutedEventArgs e)
+        {
+            // Validate input
+            if (string.IsNullOrWhiteSpace(tbxFolder.Text))
+            {
+                Utils.TaskDialogWarning("Validation Error", "Delete Backups", "Please select a folder.");
+                return;
+            }
+
+            if (!Directory.Exists(tbxFolder.Text))
+            {
+                Utils.TaskDialogWarning("Validation Error", "Delete Backups", "Selected folder does not exist.");
+                return;
+            }
+
+            // Set properties
+            SelectedFolder = tbxFolder.Text;
+            IncludeSubfolders = cbxSubFolders.IsChecked ?? false;
+
+            this.DialogResult = true;
+            this.Close();
+        }
+
+        private void btnCancel_Click(object sender, RoutedEventArgs e)
+        {
+            this.DialogResult = false;
+            this.Close();
+        }
+
+        private void btnHelp_Click(object sender, RoutedEventArgs e)
+        {
+            Process.Start("https://lifestyle-usa-design.atlassian.net/wiki/x/AYBOJ");
         }
     }
 }
